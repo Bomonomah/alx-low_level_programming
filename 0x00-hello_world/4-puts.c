c92ef7eaa74b704363b8d0c35b67a4737b5f38a0
William <williamatali152@gmail.com>
2023-03-20 19:19:39 -0700

    my first line of c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/*
+ * main - print a line of codes using puts
+ * 
+ * Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like build:::ing a multilingual puzzel"");
+	return (0);
+}
